f7572f104924bbe074163593de26975744f8bb73
Irdorat <klykov.pasha@gmail.com>
2026-04-16 14:51:29 +0800

    Первая версия на платформу Kaggle

--- notebooks/catboost_model.py
+++ notebooks/catboost_model.py
@@ -19,7 +19,7 @@ def _():
     from sklearn.compose import ColumnTransformer
     from sklearn.pipeline import Pipeline
 
-    from sklearn.metrics import accuracy_score, f1_score, roc_curve, auc, confusion_matrix, ConfusionMatrixDisplay, precision_score, recall_score, make_scorer
+    from sklearn.metrics import accuracy_score, f1_score, roc_curve, auc, confusion_matrix, ConfusionMatrixDisplay, precision_score, recall_score, make_scorer, average_precision_score, precision_recall_curve
     from scipy.sparse import hstack
     import matplotlib.pyplot as plt
     import seaborn as sns
@@ -32,10 +32,19 @@ def _():
         GridSearchCV,
         Pool,
         accuracy_score,
+        auc,
+        average_precision_score,
+        confusion_matrix,
         f1_score,
         make_scorer,
+        mo,
         np,
         pd,
+        plt,
+        precision_recall_curve,
+        precision_score,
+        recall_score,
+        roc_curve,
         train_test_split,
     )
 
@@ -51,15 +60,7 @@ def _(pd):
 
 
 @app.cell
-def _(
-    CatBoostClassifier,
-    GridSearchCV,
-    Pool,
-    f1_score,
-    make_scorer,
-    train,
-    train_test_split,
-):
+def _(Pool, train, train_test_split):
     X = train.drop(columns = ['target'])
     y = train['target']
 
@@ -82,7 +83,25 @@ def _(
         cat_features=['keyword'],
         text_features=['text']
     )
+    return X_test, X_train, test_pool, train_pool, y_test, y_train
 
+
+@app.cell
+def _(X_train):
+    X_train.info()
+    return
+
+
+@app.cell
+def _(
+    CatBoostClassifier,
+    GridSearchCV,
+    X_train,
+    f1_score,
+    make_scorer,
+    y_train,
+):
+    #Затратно и не запускать на тесте
     model = CatBoostClassifier(
         iterations = 1000,
         random_seed=17,
@@ -120,20 +139,18 @@ def _(
     print("Лучший F1 на кросс-валидации:", grid_search.best_score_)
 
     model = grid_search.best_estimator_
-    return X_test, X_train, test_pool, train_pool, y_test, y_train
+    return
 
 
 @app.cell
 def _(CatBoostClassifier, test_pool, train_pool):
     model_catboost=CatBoostClassifier(
         iterations=7000,
-        learning_rate=0.03,
-        depth=8,
-        l2_leaf_reg=5,
+        learning_rate=0.01,
+        depth=10,
+        l2_leaf_reg=7,
         auto_class_weights='Balanced',
-        border_count=128,
-        min_data_in_leaf=3,
-    
+
         task_type="GPU",
         devices="0",
 
@@ -206,6 +223,124 @@ def _(
         }
     )
     print (result)
+    return best_thresh, y_test_predict, y_test_proba, y_train_predict
+
+
+@app.cell
+def _(auc, plt, roc_curve, y_test, y_test_proba):
+    #ROC-AUC
+    fpr_logreg, tpr_logreg, _ = roc_curve(y_test, y_test_proba) #false positive rate (доля ложноположительных срабатываний), true positive rate (чувствительность recall)
+    roc_auc = auc(fpr_logreg, tpr_logreg) #AUC = 1 — идеальная классификация, 0.5 — случайная
+
+    plt.plot(fpr_logreg, tpr_logreg, label=f'Logistic Regression (AUC = {roc_auc:.3f})')
+
+    plt.plot([0,1],[0,1],'--') ## Эта линия соответствует случайной модели (AUC=0.5)
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.legend()
+    plt.show()
+    return
+
+
+@app.cell
+def _(
+    confusion_matrix,
+    pd,
+    precision_score,
+    recall_score,
+    y_test,
+    y_test_predict,
+    y_train,
+    y_train_predict,
+):
+    cm = confusion_matrix(y_test, y_test_predict) #матрица неточностей для теста, показывающая TP, FN, TN, FP
+    cm_df = pd.DataFrame(
+        cm,
+        index=['Negative', 'Positive'],
+        columns=['Predicted Negative', 'Predicted Positive']
+    )
+
+    print(cm_df)
+    print(f"Precission for train: {precision_score(y_train, y_train_predict):.3f}\nRecall for train: {recall_score(y_train, y_train_predict):.3f}\nPrecission for test: {precision_score(y_test, y_test_predict):.3f}\nRecall for test: {recall_score(y_test, y_test_predict):.3f}")
+    return
+
+
+@app.cell
+def _(plt, precision_recall_curve, y_test, y_test_proba):
+    precision, recall, t = precision_recall_curve(y_test, y_test_proba)
+
+    plt.plot(t, precision[:-1], label='Precision')
+    plt.plot(t, recall[:-1], label='Recall')
+    plt.xlabel('Threshold')
+    plt.ylabel('Score')
+    plt.legend()
+    plt.show()
+    return precision, recall
+
+
+@app.cell
+def _(average_precision_score, plt, precision, recall, y_test, y_test_proba):
+    ap = average_precision_score(y_test, y_test_proba)
+
+    # Строим PR кривую
+    plt.plot(recall, precision, 'b-', linewidth=2, label=f'PR curve (AP = {ap:.3f})')
+
+    # Настройка графика
+    plt.xlabel('Recall (Полнота)', fontsize=12)
+    plt.ylabel('Precision (Точность)', fontsize=12)
+    plt.title('Precision-Recall Curve', fontsize=14)
+    plt.legend(loc='best')
+    plt.grid(True, alpha=0.3)
+
+    plt.legend()
+    plt.show()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Выполним
+    """)
+    return
+
+
+@app.cell
+def _(pd):
+    subm_csv = pd.read_csv('data/test_ready.csv')
+    subm_csv['keyword'] = subm_csv['keyword'].fillna('None', inplace=False)
+    subm_csv = subm_csv.dropna(subset=['text'])
+    subm_csv.info()
+    return (subm_csv,)
+
+
+@app.cell
+def _(subm_csv):
+    subm_csv_X = subm_csv.drop(['id'], axis=1)
+    subm_csv_y = subm_csv[['id']]
+    subm_csv_X.head()
+    return subm_csv_X, subm_csv_y
+
+
+@app.cell
+def _(best_thresh, model_catboost, subm_csv_X):
+    subm_csv_proba = model_catboost.predict_proba(subm_csv_X)[:,1]
+    subm_csv_X['target'] = (subm_csv_proba >= best_thresh).astype(int)
+    return
+
+
+@app.cell
+def _(subm_csv_X, subm_csv_y):
+    subm_csv_X_index = subm_csv_X.reset_index()
+    subm_csv_y_index = subm_csv_y.reset_index()
+    subm_csv_merged = subm_csv_y_index.merge(
+        subm_csv_X_index, 
+        on='index', 
+        how='left')
+    subm_csv_merged = subm_csv_merged.drop('index', axis=1)
+    subm_csv_merged = subm_csv_merged.drop(['keyword', 'text', 'text_len', 'word_count', 'unique_word_rate'], axis = 1)
+    subm_csv_merged.head(20)
+    subm_csv_merged.to_csv('data/test_submission.csv', index=False, encoding='utf-8')
     return
 
 
